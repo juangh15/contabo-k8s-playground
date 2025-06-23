f3513215fe7a1d94c1839d5df4cadc31e72d2b72
Juangh15 <juansarrias15@gmail.com>
2025-06-22 19:06:56 -0500

    Fix LB

--- setup_loadbalancer/init.sh
+++ setup_loadbalancer/init.sh
@@ -3,12 +3,6 @@
 echo "Using domain: $DOMAIN"
 echo "Using email: $EMAIL"
 
-# Wait for 80
-while lsof -i :80 >/dev/null 2>&1; do
-  echo "Waiting for 80..."
-  sleep 2
-done
-
 # Use --staging for tests)
 certbot certonly --standalone --non-interactive --agree-tos --staging \
   --preferred-challenges http \
