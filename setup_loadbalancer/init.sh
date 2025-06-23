80ebc929e4b639c1363297353edfc51bf7c704f3
Juangh15 <juansarrias15@gmail.com>
2025-06-22 19:01:15 -0500

    Fix LB

--- setup_loadbalancer/init.sh
+++ setup_loadbalancer/init.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-# Usa las variables pasadas por Docker
 echo "Using domain: $DOMAIN"
 echo "Using email: $EMAIL"
 
-# Espera a que nginx esté listo (opcional pero útil)
-sleep 10
+# Wait for 80
+while lsof -i :80 >/dev/null 2>&1; do
+  echo "Waiting for 80..."
+  sleep 2
+done
 
-# Certbot en modo standalone
-certbot certonly --standalone --non-interactive --agree-tos \
-  -m "$EMAIL" -d "$DOMAIN"
+# Use --staging for tests)
+certbot certonly --standalone --non-interactive --agree-tos --staging \
+  --preferred-challenges http \
+  --http-01-port 80 \
+  --email "$EMAIL" -d "$DOMAIN" || exit 1
+
+# Mark certs are ready
+touch /certs/.ready
